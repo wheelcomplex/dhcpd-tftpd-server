fac0c816279f63abbc45a48afd6b3b13b2d095cd
Jiajin Yin <wheelcomplex@gmail.com>
2021-03-26 20:41:13 +0800

    tftp server works

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.17
+
+require github.com/pin/tftp v2.1.0+incompatible
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"runtime"
 	"strconv"
 	"strings"
@@ -17,10 +18,10 @@ import (
 )
 
 // root directory
-var rootdir string = ""
+var rootDir string = ""
 
 // upload to this directory
-var updir string = ""
+var uploadDir string = ""
 var dirsymble string = "/"
 
 var port int = 9080
@@ -35,60 +36,72 @@ func main() {
 		dirsymble = "\\"
 	}
 
-	flag.StringVar(&rootdir, "w", "", "directory to write uploaded file")
-	flag.StringVar(&updir, "w", "", "directory to write uploaded file")
+	flag.StringVar(&rootDir, "r", "", "directory to read file")
+	flag.StringVar(&uploadDir, "w", "", "directory to write uploaded file")
 	flag.IntVar(&port, "p", 69, "listening port")
 
 	flag.Parse()
 
-	if rootdir == "" {
-		rootdir = homedir + dirsymble + "tftproot"
+	if rootDir == "" {
+		rootDir = homedir + dirsymble + "tftproot"
 	}
 
-	err = os.Chdir(rootdir)
+	rootDir, err = filepath.EvalSymlinks(rootDir)
 	if err != nil {
-		err = os.MkdirAll(rootdir, 0700)
+		log.Fatalf("readlink the root directory: %s\n", err)
+	}
+
+	err = os.Chdir(rootDir)
+	if err != nil {
+		err = os.MkdirAll(rootDir, 0700)
 		if err != nil {
 			log.Fatalf("create the root directory: %s\n", err)
 		}
 		// re-try
-		err = os.Chdir(rootdir)
+		err = os.Chdir(rootDir)
 		if err != nil {
 			log.Fatalf("change to the root directory: %s\n", err)
 		}
 	}
 
-	if updir == "" {
-		updir = rootdir + dirsymble + "upload"
+	if uploadDir == "" {
+		uploadDir = rootDir + dirsymble + "upload"
 	}
 
-	err = os.Chdir(updir)
+	err = os.Chdir(uploadDir)
 	if err != nil {
-		err = os.MkdirAll(updir, 0700)
+		err = os.MkdirAll(uploadDir, 0700)
 		if err != nil {
 			log.Fatalf("create the upload directory: %s\n", err)
 		}
 		// re-try
-		err = os.Chdir(updir)
+		err = os.Chdir(uploadDir)
 		if err != nil {
 			log.Fatalf("change to the upload directory: %s\n", err)
 		}
 	}
-	upfile, err := os.Create(updir + "/.uploader.rw.check")
+
+	uploadDir, err = filepath.EvalSymlinks(uploadDir)
+	if err != nil {
+		log.Fatalf("readlink the upload directory: %s\n", err)
+	}
+
+	upfile, err := os.Create(uploadDir + "/.uploader.rw.check")
 	if err != nil {
 		log.Printf("WARNING: write to the upload directory: %s\n", err)
 	}
 	upfile.Close()
-	os.Remove(updir + "/.uploader.rw.check")
+	os.Remove(uploadDir + "/.uploader.rw.check")
 
-	log.SetPrefix(os.Args[0] + "[" + strconv.Itoa(os.Getgid()) + "] ")
+	log.SetPrefix(os.Args[0] + "[" + strconv.Itoa(os.Getpid()) + "] ")
 
-	log.Printf("%s, Listening at %s, upload to %s ...\n", " "+argline, " "), strconv.Itoa(port), updir)
+	log.Printf("Running [%s] ...\n", os.Args[0]+" "+argline)
+	log.Printf("Listening at %s, root directory [%s], upload to [%s] ...\n", strconv.Itoa(port), rootDir, uploadDir)
 
 	// use nil in place of handler to disable read or write operations
 	s := tftp.NewServer(readHandler, writeHandler)
-	s.SetTimeout(5 * time.Second)                     // optional
-	err := s.ListenAndServe(":" + strconv.Itoa(port)) // blocks until s.Shutdown() is called
+	s.SetTimeout(5 * time.Second)                    // optional
+	err = s.ListenAndServe(":" + strconv.Itoa(port)) // blocks until s.Shutdown() is called
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "server: %v\n", err)
 		os.Exit(1)
@@ -100,6 +113,7 @@ func main() {
 
 // readHandler is called when client starts file download from server
 func readHandler(filename string, rf io.ReaderFrom) error {
+	filename = rootDir + dirsymble + filename
 	log.Printf("reading [%s] ...\n", filename)
 	file, err := os.Open(filename)
 	if err != nil {
@@ -117,6 +131,7 @@ func readHandler(filename string, rf io.ReaderFrom) error {
 
 // writeHandler is called when client starts file upload to server
 func writeHandler(filename string, wt io.WriterTo) error {
+	filename = uploadDir + dirsymble + filename
 	log.Printf("writing [%s] ...\n", filename)
 	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0644)
 	if err != nil {
